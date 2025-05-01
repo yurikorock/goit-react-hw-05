b6101aea6b8d759d54fcba3b7c4c3944bb9d3804
Yura Shaplavsky <yurikorock777@gmail.com>
2025-05-01 22:48:14 +0300

    add in App Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,27 @@ import { useState, useEffect } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage/HomePage";
+import MoviesPage from "../pages/MoviesPage/MoviesPage.";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+import MovieDetailsPage from "../pages/MovieDetailsPage/MovieDetailsPage";
+import MovieCast from "./MovieCast/MovieCast";
+import MovieReviews from "./MovieReviews/MovieReviews";
 
 const App = () => {
   return (
     <div>
       <h1 className="title">Filmoteka</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/movies" element={<MoviesPage />}></Route>
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<MovieCast />}></Route>
+          <Route path="reviews" element={<MovieReviews />}></Route>
+        </Route>
+        <Route path="/movies" element={<NotFoundPage />}></Route>
+      </Routes>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 // Імпорт стилів нормалізації
 import "modern-normalize";
 //
 import "./index.css";
 import App from "./components/App";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
