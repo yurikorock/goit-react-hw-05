11952f592f4e092eac09c477b0a12695748df684
Yura Shaplavsky <yurikorock777@gmail.com>
2025-05-08 18:33:02 +0300

    start MovieList

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,8 +1,22 @@
+import MovieList from "../../components/MovieList/MovieList";
+import { useState } from "react";
+
 export default function MoviesPage() {
+  const [movies, setMovies] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const newQuery = form.elements.query.value.trim();
+    console.log(newQuery);
+  };
   return (
     <div>
-      <h1>Непопулярні фільми</h1>
-      <ul>666666</ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="query" />
+        <button type="submit">Search</button>
+      </form>
+      <MovieList movies={movies} />
     </div>
   );
 }
